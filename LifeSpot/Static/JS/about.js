a66e53a1be97e91b0aacc9a4cee5a0f83a53b1da
RomanSachuck <roman.sachuk.1997@mail.ru>
2024-09-13 10:02:10 +0300

    .

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -24,3 +24,25 @@ function getReview() {
 
     writeReview(review)
 }
+
+var indexValue = 1;
+showImg(indexValue)
+function btm_slide(num) { showImg(indexValue = num); }
+function side_slide(num) { showImg(indexValue += num); }
+function showImg(num) {
+    var i;
+    const img = document.querySelectorAll('img');
+    const slider = document.querySelectorAll('.btm-slides span');
+    if (num > img.length)
+        indexValue = 1;
+    if (num < 1)
+        indexValue = img.length;
+    for (i = 0; i < img.length; i++) {
+        img[i].style.display = "none";
+    }
+    for (i = 0; i < slider.length; i++) {
+        slider[i].style.background = "rgba(255,255,255,0.2)";
+    }
+    img[indexValue - 1].style.display = "block";
+    slider[indexValue - 1].style.background = "rgba(255,255,255,0.7)";
+}
